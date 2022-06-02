3fc96f5b97ed3ee5d486d2d25a73d362118624d3
Ndudirim <ndustan4gmail.com>
2022-06-02 06:00:38 -0400

    Added First Java Project

---
+++ First_Project.java
@@ -0,0 +1,13 @@
+public class First_Project {
+
+    public static void main(String[] args){
+    System.out.println("Hello World!");
+        System.out.println("This is my First Project.");
+        System.out.println("I will be learning how to write codes.");
+        System.out.println("This will help me greatly as an Automation Software Tester.");
+        System.out.println("I will be starting with Selenium WebDriver.");
+        System.out.println("Then, I'll venture into other ares of Test Automation that requires coding Skills.");
+        System.out.println("I will become an expert soon, the world will be looking for me.");
+
+    }
+}
